fd1aa7eae9772ae231efd1b5dc6757aa28a49169
Mifdzal Irfan <mifdzalirfan23@gmail.com>
2025-05-22 00:18:18 +0900

    Initial springboot setup

---
+++ src/main/java/dongyang/krac/IrfanFinalProject/IrfanFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package dongyang.krac.IrfanFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrfanFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrfanFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dongyang/krac/IrfanFinalProject/IrfanFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dongyang.krac.IrfanFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrfanFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
